69ad3a414a404effdb0bde68fa52299581404ce0
Jake Dombrower <jakedombrowerdroid@gmail.com>
2013-12-21 17:05:12 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ObsidianFoundation::Application.config.secret_key_base = '1a6d4145925cca5f7ea3fe0d1a1bf14d1114921f32734eea951ec2edae30e0c1f1074ea9510368b1ccd32949ff8f530c04874d1921da6ff02f21b8e84e22338c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ObsidianFoundation::Application.config.session_store :cookie_store, key: '_obsidian-foundation_session'
